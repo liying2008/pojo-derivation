f0ecb2db1487fa286c8bc4e425ae54b761e2a710
liying2008 <liruoer2008@yeah.net>
2021-02-16 14:57:01 +0800

    convert sample build.gradle to build.gradle.kts

---
+++ sample/sample-base/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm")
+    kotlin("kapt")
+}
+
+group = ext["LIB_GROUP"]!!
+version = ext["LIB_VERSION"]!!
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    compileOnly(project(":pojo-derivation-annotations"))
+    kapt(project(":pojo-derivation-compiler"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation(group = "junit", name = "junit", version = "4.12")
+}
+
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "pojo-derivation"
+include(
+    "pojo-derivation-annotations",
+    "pojo-derivation-compiler",
+    "sample:sample-base",
+    "sample:sample-1"
+)
